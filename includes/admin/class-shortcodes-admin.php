70b55e22ac39557b2789659521f50bcf2292571b
shinTM <emasugeta@gmail.com>
2017-02-12 16:56:45 +0200

    upd: section shortcode settings

--- includes/admin/class-shortcodes-admin.php
+++ includes/admin/class-shortcodes-admin.php
@@ -52,6 +52,7 @@ class Cherry_Shortcodes_Admin {
 	 */
 	public function shortcode_registration() {
 		//$utility = cherry_site_shortcodes()->get_core()->modules['cherry-utility']->utility;
+
 		// Grid Shortcodes list
 		cherry5_register_shortcode(
 				array(
@@ -63,109 +64,16 @@ class Cherry_Shortcodes_Admin {
 				)
 		);
 
+		// Base Shortcodes list
 		cherry5_register_shortcode(
 				array(
 					'title'       => esc_html__( 'Basic Shortcodes', 'cherry-site-shortcodes' ),
 					'description' => esc_html__( 'Base cherry shortcode collection', 'cherry-site-shortcodes' ),
 					'icon'        => '<span class="dashicons dashicons-admin-generic"></span>',
 					'slug'        => 'cherry-shortcodes',
-					'shortcodes'  => array(
-
-						array(
-							'title'       => esc_html__( 'Section', 'cherry-site-shortcodes' ),
-							'description' => esc_html__( 'Shortcode is used to display the content section', 'cherry-site-shortcodes' ),
-							'icon'        => '<span class="dashicons dashicons-align-center"></span>',
-							'slug'        => 'cherry_section',
-							'enclosing'   => true,
-							'options'     => array(
-								'background_type' => array(
-									'type'          => 'radio',
-									'title'         => esc_html__( 'Background type', 'cherry-site-shortcodes' ),
-									'description'   => esc_html__( 'Choose background type for section', 'cherry-site-shortcodes' ),
-									'value'         => 'fill-color',
-									'display_input' => false,
-									'options'       => array(
-										'fill-color' => array(
-											'label' => esc_html__( 'Fill color', 'cherry-site-shortcodes' ),
-										),
-										'image' => array(
-											'label' => esc_html__( 'Image', 'cherry-site-shortcodes' ),
-										),
-									),
-								),
-								'background_color' => array(
-									'type'        => 'colorpicker',
-									'title'       => esc_html__( 'Background fill color', 'cherry-site-shortcodes' ),
-									'description' => esc_html__( 'Description color picker.', 'cherry-site-shortcodes' ),
-									'value'       => '',
-								),
-								'background_opacity' => array(
-									'type'        => 'slider',
-									'title'       => esc_html__( 'Background opacity', 'cherry-site-shortcodes' ),
-									'description' => esc_html__( 'Select value for background opacity(Fill color mode)', 'cherry-site-shortcodes' ),
-									'max_value'   => 100,
-									'min_value'   => 0,
-									'value'       => 100,
-								),
-								'background_image' => array(
-									'type'               => 'media',
-									'title'              => esc_html__( 'Background image', 'cherry-site-shortcodes' ),
-									'description'        => esc_html__( 'Select background image using media library', 'cherry-site-shortcodes' ),
-									'value'              => '',
-									'multi_upload'       => false,
-									'library_type'       => 'image',
-									'upload_button_text' => esc_html__( 'Choose image', 'cherry-site-shortcodes' ),
-								),
-								'background_size' => array(
-									'type'          => 'radio',
-									'title'         => esc_html__( 'Background size', 'cherry-site-shortcodes' ),
-									'description'   => esc_html__( 'Choose background size for section image background', 'cherry-site-shortcodes' ),
-									'value'         => 'cover',
-									'display_input' => false,
-									'options'       => array(
-										'cover' => array(
-											'label' => esc_html__( 'Cover', 'cherry-site-shortcodes' ),
-										),
-										'cover-left' => array(
-											'label' => esc_html__( 'Cover left', 'cherry-site-shortcodes' ),
-										),
-										'cover-right' => array(
-											'label' => esc_html__( 'Cover right', 'cherry-site-shortcodes' ),
-										),
-									),
-								),
-								'padding_top' => array(
-									'type'        => 'slider',
-									'title'       => esc_html__( 'Padding top', 'cherry-site-shortcodes' ),
-									'description' => esc_html__( 'Select value for padding-top property', 'cherry-site-shortcodes' ),
-									'max_value'   => 500,
-									'min_value'   => 0,
-									'value'       => 0,
-								),
-								'padding_bottom' => array(
-									'type'        => 'slider',
-									'title'       => esc_html__( 'Padding bottom', 'cherry-site-shortcodes' ),
-									'description' => esc_html__( 'Select value for padding-bottom property', 'cherry-site-shortcodes' ),
-									'max_value'   => 500,
-									'min_value'   => 0,
-									'value'       => 0,
-								),
-								'class' => array(
-									'type'        => 'text',
-									'title'       => esc_html__( 'Custom class', 'cherry-site-shortcodes' ),
-									'description' => esc_html__( 'Assign cusstom class for for section', 'cherry-site-shortcodes' ),
-									'value'       => '',
-									'placeholder' => esc_html__( 'Input class', 'cherry-site-shortcodes' ),
-									'class'       => '',
-								),
-
-							),
-						),
-
-					),
+					'shortcodes'  => $this->base_shortcodes_settings,
 				)
 			);
-
 	}
 
 	/**
--- includes/public/shortcodes/section.php
+++ includes/public/shortcodes/section.php
@@ -34,6 +34,108 @@ class Cherry_Section_Shortcode extends Cherry_Main_Shortcode {
 		$this->name = 'section';
 
 		parent::__construct();
+
+		if ( is_admin() ) {
+			add_action( 'after_setup_theme', array( $this, 'shortcode_registration' ), 9 );
+		}
+	}
+
+	/**
+	 * Define fields settings.
+	 *
+	 * @return viod
+	 */
+	public function shortcode_registration() {
+		cherry_shortcodes_admin()->base_shortcodes_settings[] = array(
+			'title'       => esc_html__( 'Section', 'cherry-site-shortcodes' ),
+			'description' => esc_html__( 'Shortcode is used to display the content section', 'cherry-site-shortcodes' ),
+			'icon'        => '<span class="dashicons dashicons-align-center"></span>',
+			'slug'        => 'cherry_section',
+			'enclosing'   => true,
+			'options'     => array(
+				'background_type' => array(
+					'type'          => 'radio',
+					'title'         => esc_html__( 'Background type', 'cherry-site-shortcodes' ),
+					'description'   => esc_html__( 'Choose background type for section', 'cherry-site-shortcodes' ),
+					'value'         => 'fill-color',
+					'display_input' => false,
+					'options'       => array(
+						'fill-color' => array(
+							'label' => esc_html__( 'Fill color', 'cherry-site-shortcodes' ),
+						),
+						'image' => array(
+							'label' => esc_html__( 'Image', 'cherry-site-shortcodes' ),
+						),
+					),
+				),
+				'background_color' => array(
+					'type'        => 'colorpicker',
+					'title'       => esc_html__( 'Background fill color', 'cherry-site-shortcodes' ),
+					'description' => esc_html__( 'Description color picker.', 'cherry-site-shortcodes' ),
+					'value'       => '',
+				),
+				'background_opacity' => array(
+					'type'        => 'slider',
+					'title'       => esc_html__( 'Background opacity', 'cherry-site-shortcodes' ),
+					'description' => esc_html__( 'Select value for background opacity(Fill color mode)', 'cherry-site-shortcodes' ),
+					'max_value'   => 100,
+					'min_value'   => 0,
+					'value'       => 100,
+				),
+				'background_image' => array(
+					'type'               => 'media',
+					'title'              => esc_html__( 'Background image', 'cherry-site-shortcodes' ),
+					'description'        => esc_html__( 'Select background image using media library', 'cherry-site-shortcodes' ),
+					'value'              => '',
+					'multi_upload'       => false,
+					'library_type'       => 'image',
+					'upload_button_text' => esc_html__( 'Choose image', 'cherry-site-shortcodes' ),
+				),
+				'background_size' => array(
+					'type'          => 'radio',
+					'title'         => esc_html__( 'Background size', 'cherry-site-shortcodes' ),
+					'description'   => esc_html__( 'Choose background size for section image background', 'cherry-site-shortcodes' ),
+					'value'         => 'cover',
+					'display_input' => false,
+					'options'       => array(
+						'cover' => array(
+							'label' => esc_html__( 'Cover', 'cherry-site-shortcodes' ),
+						),
+						'cover-left' => array(
+							'label' => esc_html__( 'Cover left', 'cherry-site-shortcodes' ),
+						),
+						'cover-right' => array(
+							'label' => esc_html__( 'Cover right', 'cherry-site-shortcodes' ),
+						),
+					),
+				),
+				'padding_top' => array(
+					'type'        => 'slider',
+					'title'       => esc_html__( 'Padding top', 'cherry-site-shortcodes' ),
+					'description' => esc_html__( 'Select value for padding-top property', 'cherry-site-shortcodes' ),
+					'max_value'   => 500,
+					'min_value'   => 0,
+					'value'       => 0,
+				),
+				'padding_bottom' => array(
+					'type'        => 'slider',
+					'title'       => esc_html__( 'Padding bottom', 'cherry-site-shortcodes' ),
+					'description' => esc_html__( 'Select value for padding-bottom property', 'cherry-site-shortcodes' ),
+					'max_value'   => 500,
+					'min_value'   => 0,
+					'value'       => 0,
+				),
+				'class' => array(
+					'type'        => 'text',
+					'title'       => esc_html__( 'Custom class', 'cherry-site-shortcodes' ),
+					'description' => esc_html__( 'Assign cusstom class for for section', 'cherry-site-shortcodes' ),
+					'value'       => '',
+					'placeholder' => esc_html__( 'Input class', 'cherry-site-shortcodes' ),
+					'class'       => '',
+				),
+			),
+
+		);
 	}
 
 	/**
